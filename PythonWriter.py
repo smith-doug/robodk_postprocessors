421a3823b6aac0dd7115c55d9430ef339bcc63f9
Doug Smith <douglas.smith@swri.org>
2023-02-17 12:15:44 -0600

    PythonWriter can create and save a runnable program

--- PythonWriter.py
+++ PythonWriter.py
@@ -1,29 +1,32 @@
 from __future__ import annotations
 
 import functools
+import os
 from dataclasses import dataclass, field
 import inspect
 from .robodk import *
 from .BasePost import BasePost, BaseRoboDKConfig
-from functools import wraps
+from pathlib import Path
 
 
 # https://stackoverflow.com/a/73312204
-def function_details(f):
+def function_details(f, *, indent=True):
     assert (callable(f))
 
     @functools.wraps(f)
     def wrapper(*args, **kwargs):
+        prefix = "\t" if indent else ""
+
         siggy = inspect.signature(f)
         bound_siggy = siggy.bind(*args, **kwargs)
-        bound_siggy.arguments.pop('self')
+        bound_siggy.arguments.pop('self')  # Remove self from args for display
         all_sargs = ", ".join(kw + "=" + repr(arg) for kw, arg in bound_siggy.arguments.items())
-        obj: BasePost = args[0]
-
-        outstr = f"robot.{f.__name__}({all_sargs})"
-        # print("robot.", f.__name__, "(", all_sargs, ")", sep="")
+        outstr = f"{prefix}robot.{f.__name__}({all_sargs})"
         print(outstr)
-        obj.PROG.append(outstr)
+        # Add the line to the RobotPost object
+        obj: BasePost = args[0]
+        obj.addline(outstr)
+
         return f(*args, **kwargs)
 
     return wrapper
@@ -32,27 +35,75 @@ def function_details(f):
 @dataclass
 class RoboDKConfig(BaseRoboDKConfig):
     robot_post: str = "PythonWriter"
+    real_robot_post: str = ""
 
 
 class RobotPost(BasePost):
+    PROG_EXT = "py"
 
     def __init__(self, robotpost=None, robotname=None, robot_axes=6, **kwargs):
         super().__init__(robotpost, robotname, robot_axes, **kwargs)
         self.PROG = []
+        self.top_lines = []
+        self.real_robot_post: str = ""
+        self.config: dict[str, any] = {}
+
+    @classmethod
+    def from_config(cls, config: BaseRoboDKConfig | dict) -> BasePost:
+        ret = super().from_config(config)
+        ret.config = config
+        return ret
+
+    def _add_python_header(self):
+        top_lines: list[str] = []
+
+        file_loc = inspect.getfile(type(self))
+        file_path = Path(file_loc)
+        dir_path = file_path.parent.parent
+
+        top_lines.append("import sys\nimport os")
+        top_lines.append(f"sys.path.insert(0, '{dir_path}')")
+        top_lines.append(f"from robodk_postprocessors.{self.real_robot_post} import RobotPost\n\n")
+        top_lines.append('def run_post():')
+        top_lines.append(f"\trobot = RobotPost('{self.real_robot_post}', '{self.ROBOT_NAME}',"
+                         f" {self.nAxes})")
+
+        # Add necessary config entries, remove the basic ones
+        cfg_to_remove = ['real_robot_post', 'robot_post', 'robot_name', 'robot_axes']
+        extra_cfg = {k: v for k, v in self.config.items() if k not in cfg_to_remove}
+        for k, v in extra_cfg.items():
+            top_lines.append(f"\trobot.{k} = {repr(v)}")
+
+        self.top_lines = top_lines
 
     @function_details
     def ProgStart(self, progname):
-        print('')
         pass
 
     @function_details
     def ProgFinish(self, progname):
         pass
 
-    @function_details
     def ProgSave(self, folder, progname, ask_user=False, show_result=False):
-        # raise Exception('###@todo, must actually save')
-        pass
+        outdir = Path(folder)
+        outdir.mkdir(exist_ok=True)
+        outfile = Path(f'{folder}/{progname}.{self.PROG_EXT}')
+
+        self._add_python_header()
+
+        with outfile.open(mode='w') as f:
+            for line in self.top_lines:
+                f.write(line + "\n")
+            for line in self.PROG:
+                f.write(line + "\n")
+
+            f.write(f"\trobot.ProgSave('{folder}', '{progname}')\n")
+
+            f.write('if __name__== "__main__":\n')
+            f.write('\trun_post()')
+
+        self.PROG = []
+        self.top_lines = []
 
     @function_details
     def MoveJ(self, pose, joints, conf_RLF=None):
@@ -114,9 +165,8 @@ class RobotPost(BasePost):
     def RunMessage(self, message, iscomment=False):
         pass
 
-    @function_details
-    def addline(self, newline):
-        pass
+    def addline(self, newline: str):
+        self.PROG.append(newline)
 
     @function_details
     def addlog(self, newline):
