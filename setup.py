c8882ea65b59076228b0b8bc4768aead353de39e
Douglas Smith <douglas.smith@swri.org>
2023-09-05 12:14:35 -0500

    Update setup.py with requirements

--- setup.py
+++ setup.py
@@ -2,8 +2,10 @@ from setuptools import find_packages, setup
 
 setup(
     name='robodk_postprocessors',
+    version='0.0.1',
     author='RoboDK',
     license='Apache-2.0',
     packages=find_packages(),
     url='https://robodk.com/post-processors',
+    install_requires=['dataclass_wizard', 'numpy']
 )
